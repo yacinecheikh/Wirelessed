9a580821479be6b5a8409f950738c16611cf36ab
Yacine Cheikhrouhou <yc303894@ad.u-bourgogne.fr>
2022-11-18 14:41:54 +0100

    successfully decoded a sector, need formatting

--- mafenetre.cpp
+++ mafenetre.cpp
@@ -38,6 +38,7 @@ void MaFenetre::on_connect_btn_clicked() {
         status = Version(&reader);
         ui->device_label->setText(reader.version);
         ui->device_label->update();
+        RF_Power_Control(&reader, true, 0);
     }
     qDebug() << "OpenCOM " << status;
 }
@@ -70,12 +71,28 @@ void MaFenetre::on_card_btn_clicked()
     qDebug() << (status == MI_OK);
     if (status == MI_OK) {
         //uint8_t data[16]; // 16 bytes per block
-        uint8_t data[240];
+        uint8_t data[240]; // in a 15 blocks sector, 15 * 16bytes=240
         // read name
-        status = Mf_Classic_Read_Sector(&reader, true, 0, data, true, 0); // use key A for sector 1, stored in slot 1
+        // data is read for sector 0 with key 0
+        //status = Mf_Classic_Read_Sector(&reader, true, 0, data, true, 0);
+        // encrypted sectors are the 2, 3 and 4
+        status = Mf_Classic_Read_Sector(&reader, true, 2, data, true, 2); // use key A for sector n, with key stored in slot n
         qDebug() << (status == MI_OK);
+        if (status == MI_OK) {
+            // 3 blocks
+            printf("reading block 1 in sector 1");
+            int block = 0;
+            for (int offset = 0; offset < 16; offset++) {
+                printf("%c", data[block * 16 + offset]);
+            }
+        }
 
         // read points
+        status = Mf_Classic_Read_Sector(&reader, true, 2, data, true, 2);
+        qDebug() << (status == MI_OK);
+        if (status == MI_OK) {
+            // 3 blocks to read
+        }
         // read sectors (using key 1, 2 and 3)
         //status = Mf_Classic_Read_Block(&reader, TRUE, 0, data, true, 0);
         //status = Mf_Classic_Read_Block(&reader, TRUE, 5, data, true, 0);
